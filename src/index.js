37c85f7d0d371ce82b96c8a13346fbd1c9bb2ec6
pavlo-sheremet-dev <goit.pavel.sheremet@gmail.com>
2023-06-12 01:04:32 +0300

    save

--- src/index.js
+++ src/index.js
@@ -10,12 +10,12 @@ import 'modern-normalize';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/test">
+    <BrowserRouter basename="/test">
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
           <App />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );

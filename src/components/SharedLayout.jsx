9b3477e2246ad90f4356183004735d246102ffcc
pavlo-sheremet-dev <goit.pavel.sheremet@gmail.com>
2023-06-12 10:35:49 +0300

    save

--- src/components/SharedLayout.jsx
+++ src/components/SharedLayout.jsx
@@ -1,13 +1,14 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import AppBar from './AppBar/AppBar';
 import { Suspense } from 'react';
 import FallBackLoader from './FallBackLoader/FallBackLoader';
 
 const SharedLayout = () => {
+  const location = useLocation();
   return (
     <>
       <AppBar />
-      <Suspense fallback={<FallBackLoader />}>
+      <Suspense fallback={<FallBackLoader />} key={location.key}>
         <Outlet />
       </Suspense>
     </>

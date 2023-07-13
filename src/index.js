32bd84c29f56e11303451e09401671e2a1c4a8ef
pavlo-sheremet-dev <goit.pavel.sheremet@gmail.com>
2023-07-13 15:40:27 +0300

    change homepage

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/movie-react-goit-hw-05">
+    <BrowserRouter basename="/test">
       <App />
     </BrowserRouter>
   </React.StrictMode>
